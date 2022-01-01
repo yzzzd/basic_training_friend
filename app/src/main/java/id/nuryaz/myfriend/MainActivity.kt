ee528f267f0c5abcca9dce704bb9ce18abe9c8f2
Zidd <nuvyazid@gmail.com>
2022-01-01 14:23:53 +0700

    scroll and dialog

--- app/src/main/java/id/nuryaz/myfriend/MainActivity.kt
+++ app/src/main/java/id/nuryaz/myfriend/MainActivity.kt
@@ -2,10 +2,23 @@ package id.nuryaz.myfriend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendEmail(view: View) {
+        AlertDialog
+            .Builder(this)
+            .setTitle("Mengirim Email")
+            .setMessage("Kamu menekan tombol kirim email")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
