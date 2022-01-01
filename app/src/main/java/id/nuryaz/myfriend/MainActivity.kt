9ae58b83c5044fdbca6a7a11c70cd8ab7787ac0c
Zidd <nuvyazid@gmail.com>
2022-01-01 15:39:41 +0700

    lifecycle

--- app/src/main/java/id/nuryaz/myfriend/MainActivity.kt
+++ app/src/main/java/id/nuryaz/myfriend/MainActivity.kt
@@ -2,10 +2,24 @@ package id.nuryaz.myfriend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.nuryaz.myfriend.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        counter += 1
+        binding.tvCounter.text = "$counter"
     }
 }
